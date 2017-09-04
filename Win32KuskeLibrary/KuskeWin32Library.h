3e4b70c45730f118000b37f17b47091f495a81aa
Kuske <lucky.777yusuke@gmail.com>
2017-09-05 05:52:29 +0900

    一応クラス分けした(未完成)

--- Win32KuskeLibrary/KuskeWin32Library.h
+++ Win32KuskeLibrary/KuskeWin32Library.h
@@ -13,3 +13,81 @@ bool Kuske_Init(int width, int height, int startX, int startY);
 void Kuske_End();
 int GetEndStatus();
 bool ProcessMessage();
+
+namespace KuskeWin32Library
+{
+
+	struct WindowSize
+	{
+		int Width;
+		int Height;
+	};
+
+
+	static class WindowStatus
+	{
+	public:
+		WindowStatus() = delete;
+		~WindowStatus() = delete;
+
+		static class Size
+		{
+		public:
+			Size() = delete;
+			~Size() = delete;
+
+			static void SetWindowSize(int width, int height);
+			static WindowSize GetWindowSize();
+
+			static void ReGetWindowSize();
+
+		private:
+			static int WindowWidth;
+			static int WindowHeight;
+		};
+
+	};
+
+	//////////////////////////////////////////////////////////////////////////////
+
+
+	class Music
+	{
+	public:
+		Music(std::string FileName, std::string MusicType);
+		~Music();
+
+		static class PlayMusicMode
+		{
+		public:
+			PlayMusicMode() = delete;
+			~PlayMusicMode() = delete;
+
+		};
+
+		static class MusicType
+		{
+		public:
+			MusicType() = delete;
+			~MusicType() = delete;
+
+
+
+
+		private:
+
+		};
+
+
+		bool PlayMusic(int PlayMode);
+
+
+	private:
+
+		MCI_OPEN_PARMS mop;
+
+
+	};
+
+
+};
